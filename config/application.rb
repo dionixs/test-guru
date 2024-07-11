82939198507c546aece7c2f088cf91074f6c5a59
di0nixs <di0niixs@gmail.com>
2024-07-11 16:25:17 +0300

    Feat(lang): add default locale

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     config.time_zone = "Moscow"
+    config.i18n.default_locale = :ru
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
