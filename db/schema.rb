a3e13ee5c0302d8035b2bb7cfaa813688d3b9e4e
di0nixs <di0niixs@gmail.com>
2024-07-15 16:19:45 +0300

    Add level default to tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_124445) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_124907) do
   create_table "answers", force: :cascade do |t|
     t.string "value", null: false
     t.boolean "correct", default: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_124445) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
