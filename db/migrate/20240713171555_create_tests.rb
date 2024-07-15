9b1fa5c22ba16a0c739f0ed85c3c7386e026fcbc
di0nixs <di0niixs@gmail.com>
2024-07-15 15:53:34 +0300

    Add Test model

---
+++ db/migrate/20240713171555_create_tests.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTests < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tests do |t|
+      t.string :title
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
