1c5579ce5203c7b26a745bbe11b30a094dcdf51d
di0nixs <di0niixs@gmail.com>
2024-07-15 16:12:11 +0300

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_124040) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_124334) do
   create_table "answers", force: :cascade do |t|
     t.string "value", null: false
     t.boolean "correct", default: false
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_124040) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
