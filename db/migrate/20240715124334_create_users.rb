c039d1baf15bbf128c56e8eeeee718fa2740de04
di0nixs <di0niixs@gmail.com>
2024-07-15 16:12:11 +0300

    Add User model

---
+++ db/migrate/20240715124334_create_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password
+      t.boolean :admin, default: false
+
+      t.timestamps
+    end
+  end
+end
