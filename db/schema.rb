9091c220f593d7685d080eb1b54b130af129fc8f
di0nixs <di0niixs@gmail.com>
2024-07-15 22:41:10 +0300

    Fix migrations: destroy overhead migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_124907) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_124445) do
   create_table "answers", force: :cascade do |t|
     t.string "value", null: false
     t.boolean "correct", default: false
