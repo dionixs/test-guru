1779975c12b87be1781d8a69813a13c6f67061a3
di0nixs <di0niixs@gmail.com>
2024-07-16 21:52:44 +0300

    Fix reference to users

--- db/migrate/20240716105231_create_tests.rb
+++ db/migrate/20240716105231_create_tests.rb
@@ -7,7 +7,7 @@ class CreateTests < ActiveRecord::Migration[7.1]
       t.integer :level, null: false, default: 0
 
       t.references :category, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
