5a1e7a83060e8ce40c15bf70730abde9be872382
di0nixs <di0niixs@gmail.com>
2024-07-16 21:41:22 +0300

    Add null constraint for test level

--- db/migrate/20240716105231_create_tests.rb
+++ db/migrate/20240716105231_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[7.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default: 0
+      t.integer :level, null: false, default: 0
 
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
