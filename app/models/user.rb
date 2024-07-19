377af066f24c0b21f59ed9e87dfed7936a4cbe3f
di0nixs <di0niixs@gmail.com>
2024-07-20 00:04:24 +0300

    Add tests_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+
+  # метод принимает в качестве аргумента значение уровня сложности
+  # и возвращает список всех Тестов, которые проходит
+  # или когда-либо проходил Пользователь на этом уровне сложности
+  def tests_by_level(level)
+    Test.find_by_sql(['
+      SELECT *
+      FROM tests
+      INNER JOIN user_tests ON user_tests.test_id = tests.id
+      WHERE level = ? AND user_id = ?', level, self.id
+    ])
+  end
 end
