d00e1c9259831d2b75e6db9491ac73352c1f7b8c
di0nixs <di0niixs@gmail.com>
2024-07-21 15:06:01 +0300

    Refactoring tests_by_level method: use AR methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,7 @@ class User < ApplicationRecord
   # и возвращает список всех Тестов, которые проходит
   # или когда-либо проходил Пользователь на этом уровне сложности
   def tests_by_level(level)
-    Test.find_by_sql(['
-      SELECT *
-      FROM tests
-      INNER JOIN user_tests ON user_tests.test_id = tests.id
-      WHERE level = ? AND user_id = ?', level, self.id
-    ])
+    Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id')
+        .where('level = :level AND user_tests.user_id = :id', level:, id:)
   end
 end
