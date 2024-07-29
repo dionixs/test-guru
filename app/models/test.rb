69c8c04c2ae1b975df5ba1cb1c963f37edfc20aa
di0nixs <di0niixs@gmail.com>
2024-07-29 13:13:03 +0300

    Refactoring test_names_by_category method: refactoring .joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -34,9 +34,7 @@ class Test < ApplicationRecord
   # названий всех Тестов у которых Категория называется определённым образом
   # (название категории передается в метод в качестве аргумента).
   def self.test_names_by_category(title)
-    joins('INNER JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: })
-      .order(title: :desc)
-      .pluck(:title)
+    joins(:category).where(categories: { title: })
+      .order(title: :desc).pluck(:title)
   end
 end
