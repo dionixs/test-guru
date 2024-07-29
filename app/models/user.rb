4b089c57644f9b8cb43efd2a3b2bf46bf17efe45
di0nixs <di0niixs@gmail.com>
2024-07-29 13:14:00 +0300

    Refactoring tests_by_level method: refactoring .joins

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   # и возвращает список всех Тестов, которые проходит
   # или когда-либо проходил Пользователь на этом уровне сложности
   def tests_by_level(level)
-    Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id')
+    Test.joins(:user_tests)
         .where(level:, user_tests: { user_id: id })
   end
 end
