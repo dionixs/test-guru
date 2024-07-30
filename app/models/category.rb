58db818d27ae15491010eeceb371f9e46440bf8d
di0nixs <di0niixs@gmail.com>
2024-07-30 18:08:14 +0300

    Add default scope at start

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,9 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Category < ApplicationRecord
+  default_scope -> { order(title: :asc) }
+
   has_many :tests, dependent: :delete_all
 
   validates :title, presence: true
-
-  default_scope -> { order(title: :asc) }
 end
