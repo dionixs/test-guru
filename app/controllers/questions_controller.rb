00d2dca49a1759f9467d62ac4c7103f157f53729
di0nixs <di0niixs@gmail.com>
2024-08-02 19:46:43 +0300

    Add action: index, show, new, create, destroy and create views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,50 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index show new create destroy]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    @questions = @test.questions
+  end
+
+  def show; end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.test = @test
+
+    if @question.save
+      redirect_to test_questions_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to test_questions_path(@test)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:value, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = @test.questions.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
 end
