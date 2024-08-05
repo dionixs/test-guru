eb45af914091b3efad7946c6e143fdd8ed458391
Roman Andreeff <di0niixs@gmail.com>
2024-08-05 19:46:00 +0300

    Add github_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def current_year
     Time.zone.now.year
   end
+
+  def github_url(author, repo)
+    link_to author, repo, target: '_blank', rel: 'noopener nofollow'
+  end
 end
