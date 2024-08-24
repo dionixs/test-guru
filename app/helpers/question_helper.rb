e6998fb298c628693d155501fdd050cfe2c0f54e
Roman Andreeff <di0niixs@gmail.com>
2024-08-24 14:32:39 +0300

    Update question_header

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,5 +1,6 @@
 module QuestionHelper
-  def question_header(test)
-    test.new_record? ? "Create New #{test.title} Question" : "Edit #{test.title} Question"
+  def question_header(question)
+    test = question.test
+    question.new_record? ? "Create New #{test.title} Question" : "Edit #{test.title} Question"
   end
 end
